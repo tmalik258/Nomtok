564693877c5edc0f6a8304ae0d45a3a93ede761b
Talha Malik <talhamalik25.tm@gmail.com>
2025-10-19 22:29:08 +0500

    build(frontend): add next-sitemap config and update tsconfig
    Add configuration for next-sitemap to generate sitemap and robots.txt automatically. Update tsconfig to include the new config file.

--- frontend/next-sitemap.config.js
+++ frontend/next-sitemap.config.js
@@ -1,6 +1,4 @@
-import { IConfig } from 'next-sitemap'
-
-const config: IConfig = {
+const config = {
   siteUrl: 'https://nomtok.com', // 👈 your live domain
   generateRobotsTxt: true,       // also creates robots.txt automatically
   changefreq: 'weekly',
